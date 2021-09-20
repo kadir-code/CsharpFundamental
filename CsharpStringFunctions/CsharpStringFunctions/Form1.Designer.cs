59ed06feb1764cb6e3c8e49f1020f739803fcd2a
kadir <56607309+kadir-code@users.noreply.github.com>
2021-09-20 23:10:34 +0300

    Add files via upload

---
+++ CsharpStringFunctions/CsharpStringFunctions/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace CsharpStringFunctions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtDataEnter = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(39, 62);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(146, 388);
+            this.listBox1.TabIndex = 2;
+            // 
+            // txtDataEnter
+            // 
+            this.txtDataEnter.Location = new System.Drawing.Point(39, 8);
+            this.txtDataEnter.Multiline = true;
+            this.txtDataEnter.Name = "txtDataEnter";
+            this.txtDataEnter.Size = new System.Drawing.Size(146, 31);
+            this.txtDataEnter.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(224, 12);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(146, 31);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "ToUpper()";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(224, 62);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(146, 31);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "ToLower()";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(224, 109);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(146, 31);
+            this.btn3.TabIndex = 0;
+            this.btn3.Text = "Compare()";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(225, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 31);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(-917, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 31);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Compare()";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(225, 162);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(146, 31);
+            this.btn4.TabIndex = 0;
+            this.btn4.Text = "CompareTo()";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(-917, 353);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(146, 31);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Compare()";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(227, 211);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(146, 31);
+            this.btn5.TabIndex = 0;
+            this.btn5.Text = "Concat()";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(-914, 455);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(146, 31);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "CompareTo()";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(226, 254);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(146, 31);
+            this.btn6.TabIndex = 0;
+            this.btn6.Text = "Contains()";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(-913, 547);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(146, 31);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "button1";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(-1869, 748);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(146, 31);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "CompareTo()";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(226, 334);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(146, 31);
+            this.btn8.TabIndex = 0;
+            this.btn8.Text = "StartsWith()";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(227, 291);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(146, 31);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "EndsWith()";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(226, 425);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(146, 31);
+            this.btn10.TabIndex = 0;
+            this.btn10.Text = "GetEnumerator()";
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.btn10_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(227, 382);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(146, 31);
+            this.btn9.TabIndex = 0;
+            this.btn9.Text = "Equals()";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 585);
+            this.Controls.Add(this.txtDataEnter);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtDataEnter;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn9;
+    }
+}
+
---
+++ CsharpStringFunctions/CsharpStringFunctions/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsharpStringFunctions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //upper all latters
+            string value = txtDataEnter.Text;
+            MessageBox.Show(value.ToUpper());
+            txtDataEnter.Clear();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            //lower all letters
+            string value = txtDataEnter.Text;
+            MessageBox.Show(value.ToLower());
+            txtDataEnter.Clear();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+
+            //hello=-1
+            //hello world=0
+            //hello worldxxx=1
+            string value = txtDataEnter.Text;
+
+            var result1 = string.Compare(value, "hello world");
+            MessageBox.Show(result1.ToString());
+            txtDataEnter.Clear();
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            //if value is before f=1
+            //if value is equal to f=0
+            //if value is after f=-1
+
+            string value = txtDataEnter.Text;
+
+            var result1 = string.Compare(value, "f");
+            MessageBox.Show(result1.ToString());
+            txtDataEnter.Clear();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            //string concatinating
+            string value = txtDataEnter.Text;
+            string value2 = "lor4m ip5um";
+
+            string result = string.Concat(value, value2);
+            MessageBox.Show(result);
+            txtDataEnter.Clear();
+
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            //check if the entered string contains or not
+            string lorem = "lorem ipsum dolor sit amet";
+            string value = txtDataEnter.Text;
+
+            bool result = lorem.Contains(value);
+            MessageBox.Show(result.ToString());
+            txtDataEnter.Clear();
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            string value = "hello world";
+            string value2 = txtDataEnter.Text;
+
+            bool result = value.EndsWith(value2);
+            MessageBox.Show(result.ToString());
+            txtDataEnter.Clear();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            string value = "hello world";
+            string value2 = txtDataEnter.Text;
+
+            bool result = value.StartsWith(value2);
+            MessageBox.Show(result.ToString());
+            txtDataEnter.Clear();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            string value = "hello world";
+            string value2 = txtDataEnter.Text;
+
+            bool result = string.Equals(value,value2);
+            MessageBox.Show(result.ToString());
+            txtDataEnter.Clear();
+        }
+
+        private void btn10_Click(object sender, EventArgs e)
+        {
+            //used with while loop
+            string value = txtDataEnter.Text;
+            CharEnumerator charEnumerator = value.GetEnumerator();
+            while (charEnumerator.MoveNext())
+            {
+                listBox1.Items.Add(charEnumerator.Current.ToString());
+            }
+        }
+    }
+}
